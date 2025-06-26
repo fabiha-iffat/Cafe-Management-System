4d9d3f263fe3a78e3163bdd9c2e3d051ce82ec9c
fabiha-iffat <jomomo125@gmail.com>
2025-06-26 13:05:17 +0600

    Add files via upload

---
+++ Cafe-Management/Start.java
@@ -0,0 +1,8 @@
+import frame.CafeManagement;
+
+public class Start {
+    public static void main(String[] args) {
+        CafeManagement cafe = new CafeManagement();
+        cafe.setVisible(true);
+    }
+}
