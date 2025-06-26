99ab68e510cb0ccad967bc4e957d2ed7bec67d01
fabiha-iffat <jomomo125@gmail.com>
2025-06-26 13:05:47 +0600

    Delete Cafe-Management directory

--- Cafe-Management/Start.java
+++
@@ -1,8 +0,0 @@
-import frame.CafeManagement;
-
-public class Start {
-    public static void main(String[] args) {
-        CafeManagement cafe = new CafeManagement();
-        cafe.setVisible(true);
-    }
-}
